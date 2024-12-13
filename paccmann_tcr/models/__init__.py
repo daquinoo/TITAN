5aa69375efcd08a8a69f09beab5b60a783b58861
daquinoo <95264544+daquinoo@users.noreply.github.com>
2024-12-12 21:40:32 -0700

    Update __init__.py

--- paccmann_tcr/models/__init__.py
+++ paccmann_tcr/models/__init__.py
@@ -1,8 +1,10 @@
 from .knn import knn
 from .titan_multiscale import TITANMultiScale
+from .paccmann_predictor.models import BimodalMCA
 
 # More models could follow
 MODEL_FACTORY = {
     'knn': knn,
-    'bimodal_mca_multiscale': TITANMultiScale,
+    'bimodal_mca': BimodalMCA,
+    'bimodal_mca_multiscale': TITANMultiScale
 }
